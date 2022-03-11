696aebcbde33a0c0f38eaf19f41a673018ba64e3
dai <daichi.iphone@icloud.com>
2022-03-12 02:20:46 +0900

    parse(str) is able to delete blank line

--- parse.py
+++ parse.py
@@ -5,7 +5,8 @@ def parse(text: str):
     text2 = text.replace("@" + My.at_name, '')
     text_list = text2.split('\n')
     text_list2 = [t.strip() for t in text_list]
-    return text_list2
+    text_list3 = [t for t in text_list2 if t != ""]
+    return text_list3
 
 if __name__ == "__main__":
     print(parse(" @RandomValueBot abc \n def "))
