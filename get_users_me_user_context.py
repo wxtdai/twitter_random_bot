0d5185586d9e1dad033b73d33795c12ea3d26e6f
dai <daichi.iphone@icloud.com>
2022-03-12 01:49:36 +0900

    'func get_user_id()' -> 'class My_info'

--- get_users_me_user_context.py
+++ get_users_me_user_context.py
@@ -2,7 +2,6 @@ from requests_oauthlib import OAuth1Session
 import json
 from config import *
 
-
 def get_users_me_user_context():
     # User fields are adjustable, options include:
     # created_at, description, entities, id, location, name,
@@ -34,11 +33,22 @@ def get_users_me_user_context():
     print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False))
     return(json_response)
 
-def get_user_id():
-    s = get_users_me_user_context()["data"]["id"]
-    print(s)
-    print(type(s))
-    return s
+
+class My_info:
+    id         = None # "1501949516575961101"
+    title_name = None # "ランダムBot"
+    at_name    = None # "RandomValueBot" @を付けると英数字名前になる
+
+
+if My_info.id is None:
+    print("descript My_info")
+    json_response_data = (get_users_me_user_context())["data"]
+    My_info.id         = json_response_data["id"]
+    My_info.title_name = json_response_data["name"]
+    My_info.at_name    = json_response_data["username"]  
+else:
+    print("already descripted My_info")
+
 
 if __name__ == "__main__":
-    get_user_id()
+    print(My_info.at_name)
--- search_mentions.py
+++ search_mentions.py
@@ -2,39 +2,20 @@
 import requests
 import json
 from config import *
-from get_users_me_user_context import *
+from get_users_me_user_context import My_info as My
 
-
-# To set your environment variables in your terminal run the following line:
-# export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = BEARER_TOKEN
 
-
 def create_url(user_id):
-    # Replace with user ID below
     return "https://api.twitter.com/2/users/{}/mentions".format(user_id)
 
-
 def get_params():
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, author_id, context_annotations,
-    # conversation_id, created_at, entities, geo, id,
-    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-    # source, text, and withheld
     return {"tweet.fields": "created_at,public_metrics"}
 
-
 def bearer_oauth(r):
-    """
-    Method required by bearer token authentication.
-    """
-
     r.headers["Authorization"] = f"Bearer {bearer_token}"
     r.headers["User-Agent"] = "v2UserMentionsPython"
     return r
-
 
 def connect_to_endpoint(url, params):
     response = requests.request("GET", url, auth=bearer_oauth, params=params)
@@ -47,18 +28,17 @@ def connect_to_endpoint(url, params):
         )
     return response.json()
 
-
 def search_mentions(user_id):
     url = create_url(user_id)
     params = get_params()
     json_response = connect_to_endpoint(url, params)
     print("search_mentions")
     print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False))
-    return json_response
+    return json_response["data"]
 
 def search_my_mentions():
     print("search_my_mentions")
-    return search_mentions(get_user_id())
+    return search_mentions(My.id)
 
 if __name__ == "__main__":
     search_my_mentions()
