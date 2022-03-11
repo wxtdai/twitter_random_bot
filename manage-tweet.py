f17b9ffae57024b6246fc88c6b2e06a09de75fbb
dai <daichi.iphone@icloud.com>
2022-03-11 14:44:18 +0900

    support 'Read and Write Access Token'

--- manage-tweet.py
+++ manage-tweet.py
@@ -2,53 +2,18 @@ from requests_oauthlib import OAuth1Session
 import os
 import json
 from config import *
+import datetime
 
-def post():
+# https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Manage-Tweets/create_tweet.py
+def post(): # ツイートを行うが毎回認証が必要 -> じゃなくなった
+    print("post")
     # テキストをツイートしたいテキストに置き換えることを必ず追加してください。また、パラメータを追加することで、投票の投稿、ツイートの引用、返信設定付きツイート、スーパーフォロワーへのツイートなどの機能を追加することができます。
-    payload = {"text": "ここのに書いた文章が投稿されます。"}
+    payload = {"text": "[twitter bot 練習中]ここに書いた文章がTwitter上に投稿されます。" + str(datetime.datetime.now())}
 
-    # Get request token
+    # Get "request token"
     request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
-    oauth = OAuth1Session(CONSUMER_KEY, client_secret=CONSUMER_SECRET)
 
-    try:
-        fetch_response = oauth.fetch_request_token(request_token_url)
-    except ValueError:
-        print(
-            "There may have been an issue with the consumer_key or consumer_secret you entered."
-        )
-
-    resource_owner_key = fetch_response.get("oauth_token")
-    resource_owner_secret = fetch_response.get("oauth_token_secret")
-    print("Got OAuth token: %s" % resource_owner_key)
-
-    # Get authorization
-    base_authorization_url = "https://api.twitter.com/oauth/authorize"
-    authorization_url = oauth.authorization_url(base_authorization_url)
-    print("Please go here and authorize: %s" % authorization_url)
-    verifier = input("Paste the PIN here: ")
-
-    # Get the access token
-    access_token_url = "https://api.twitter.com/oauth/access_token"
-    oauth = OAuth1Session(
-        CONSUMER_KEY,
-        client_secret=CONSUMER_SECRET,
-        resource_owner_key=resource_owner_key,
-        resource_owner_secret=resource_owner_secret,
-        verifier=verifier,
-    )
-    oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-    access_token = oauth_tokens["oauth_token"]
-    access_token_secret = oauth_tokens["oauth_token_secret"]
-
-    # Make the request
-    oauth = OAuth1Session(
-        CONSUMER_KEY,
-        client_secret=CONSUMER_SECRET,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
+    oauth = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     # Making the request
     response = oauth.post(
@@ -67,5 +32,11 @@ def post():
     json_response = response.json()
     print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False))
 
+
 def main():
-    post()
+    print("main")
+    get()
+
+
+if __name__ == "__main__":
+    main()
