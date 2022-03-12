0c3be659a975a8e5bdfa9fb604cd7299e80acd1a
dai <daichi.iphone@icloud.com>
2022-03-12 13:33:45 +0900

    いいねをリプライ済の証にする方針を固め、関数の構造の変更や補足を入れた

--- exclude_replyed.py
+++ exclude_replyed.py
@@ -1,4 +1,18 @@
 
-def exclude_replyed(tweets: List[dict]):
+from typing import List
+
+from get_users_me_user_context import My_info as My
+
+def is_liked(my_id: int, tweet: dict):
+    return False
+
+def exclude_liked(tweets: List[dict]):
     # Todo: exclude process
-    return tweets 
+    non_replyed_tweets = []
+    for tweet in tweets:
+        if is_liked(My.id, tweet):
+            non_replyed_tweets.append(tweet)
+    return non_replyed_tweets
+
+def exclude_replyed(tweets: List[dict]):
+    return exclude_liked(tweets) # いいねしたもの = リプライ済 としている
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import search_mentions,my_random,parse,reply,exclude_replyed
 
 def main():
     my_mentions:List[dict] = search_mentions.search_my_mentions
-    non_replyed_mentions:List[dict] = exclude_replyed(my_mentions)
+    non_replyed_mentions:List[dict] = exclude_replyed(my_mentions) # いいねしている = リプライ済 として扱う
     for tweet in non_replyed_mentions: #tweet: dict
         parsed_tweet_content:List[str] = parse(tweet["text"])
-        reply_content = my_random(parsed_tweet_content)
-        reply(tweet, reply_content)
+        reply_content:str = my_random(parsed_tweet_content)
+        reply(tweet, reply_content) # ここでいいねもする
 
 
 if __name__ == "__main__":
