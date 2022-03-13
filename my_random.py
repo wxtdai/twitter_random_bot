2686465fd1b02f2462b856de5ea0f1133af9a7f7
dai <daichi.iphone@icloud.com>
2022-03-13 11:28:00 +0900

    ファクタリングの結果、dice_str()関数を追加

--- my_random.py
+++ my_random.py
@@ -1,17 +1,22 @@
 import random
+from typing import List
 
-def special_random(s: str):
-    num_str = ("".join(filter(str.isdigit, "2500km")))
+def dice_str(max = 6) -> str:
+    return str(random.randint(1,max))
+
+def special_random(s: str) -> str:
+    digit_only_str = ("".join(filter(str.isdigit, str)))
     if s=="サイコロ":
-        return str(random.randint(1,6))
-    elif len(num_str)>0:
-        return str(random.randint(1,int(num_str)))
+        return dice_str
+    elif len(digit_only_str)>0:
+        return str(random.randint(1,int(digit_only_str)))
     else:
         return s
 
-def my_random(l: list):
-    assert len(l)==0
-    if len(l)==1:
+def my_random(l: List[str]) -> str:
+    if len(l)==0:
+        return dice_str
+    elif len(l)==1:
         return special_random(l[0])
     else:
         return random.sample(l)
