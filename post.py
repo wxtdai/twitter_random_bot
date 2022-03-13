cb13778afc2885a2468dd47cd726eff325fbb2c3
dai <daichi.iphone@icloud.com>
2022-03-13 16:56:06 +0900

    post()のpayloadを引数にし、make_post_payload()を作成

--- post.py
+++ post.py
@@ -29,13 +29,20 @@ def post(payload: dict):
     json_response = response.json()
     print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False))
 
+def make_post_payload(text: str = "", reply_id: str = ""):
+    default_text = "[twitter bot 練習中]ここに書いた文章がTwitter上に投稿されます。" + str(datetime.datetime.now()) # Twitterは同じ文章を連投できない仕様なので時刻を入れている
+    if text == "":
+        text = default_text
+
+    payload = {
+        "text": text
+    }
+    if reply_id != "":
+        payload["reply"] = {
+            "in_reply_to_tweet_id": reply_id
+        }
+    return payload
 
 if __name__ == "__main__":
-    text = "[twitter bot 練習中]ここに書いた文章がTwitter上に投稿されます。" + str(datetime.datetime.now())
-    payload = {
-        "text": text,
-        "reply": {
-            "in_reply_to_tweet_id": "1502167033152040969"
-        }
-    }
-    post(text)
+    reply_id = "1502167033152040969"
+    post(make_post_payload(reply_id = reply_id))
