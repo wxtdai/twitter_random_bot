40227fc918d62e9eb26f9e85ca22998905fd0146
dai <daichi.iphone@icloud.com>
2022-03-13 16:59:54 +0900

    reply.pyがちゃんとリプライするようになった

--- reply.py
+++ reply.py
@@ -1,9 +1,9 @@
 
 from like import like
 from my_info import My_info
-
+from post import *
 
 def reply(tweet: dict, content: str):
+    post(make_post_payload(text=content, reply_id=tweet["id"]))
     like(My_info.id, tweet["id"])
-    pass
 
