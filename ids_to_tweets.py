8ce283baa5c9cb3d67498369ee37e2db5aa85de7
dai <daichi.iphone@icloud.com>
2022-03-14 23:33:20 +0900

    ids_to_tweets()がidsを引数に取れるように変更

--- ids_to_tweets.py
+++ ids_to_tweets.py
@@ -11,9 +11,8 @@ from utility import toCSV
 # export 'BEARER_TOKEN'='<your_bearer_token>'
 bearer_token = config.BEARER_TOKEN
 
-def create_url():
+def create_url(ids: list):
     tweet_fields = "tweet.fields=lang,author_id,public_metrics&media.fields=url" # 帰ってくるjsonに含む要素
-    ids = [1278747501642657792,1255542774432063488,1497515066232815621,1499633893104025600]
     ids_str = f"ids={toCSV(ids)}"
     
     url = "https://api.twitter.com/2/tweets?{}&{}".format(ids_str, tweet_fields)
@@ -42,12 +41,13 @@ def connect_to_endpoint(url):
     return response.json()
 
 
-def ids_to_tweets():
-    url = create_url()
+def ids_to_tweets(ids: list):
+    url = create_url(ids)
     json_response = connect_to_endpoint(url)
     print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False)) 
     # ensure_ascii=False で日本語表示
 
 
 if __name__ == "__main__":
-    ids_to_tweets()
+    ids = [1278747501642657792,1255542774432063488,1497515066232815621,1499633893104025600]
+    ids_to_tweets(ids)
