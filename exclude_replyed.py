b3ed4bd301f17c09f82a77bc1885f57d5418841c
dai <daichi.iphone@icloud.com>
2022-03-14 23:15:52 +0900

    search_liked_users.py を作成し既存の関数に利用

--- exclude_replyed.py
+++ exclude_replyed.py
@@ -1,10 +1,15 @@
 
 from typing import List
-
 from my_info import My_info as My
+from search_liked_users import search_liked_users
+from test_data import *
 
-def is_liked(my_id: int, tweet: dict):
-    return False
+def is_liked(my_id: str, tweet: dict) -> bool:
+    liked_users: List[str] = search_liked_users(tweet["id"])
+    MAX_COUNTS = 100
+    return len(liked_users) >= MAX_COUNTS or (my_id in liked_users)
+    # 100いいね以上の場合は、本来は複数回クエリを送ってuser_idを収集しなければならないのだがレアケースだしバグが起こりそうなので100いいね以上なら無条件でいいねしているものとみなします
+
 
 def exclude_liked(tweets: List[dict]):
     non_replyed_tweets = []
@@ -13,5 +18,11 @@ def exclude_liked(tweets: List[dict]):
             non_replyed_tweets.append(tweet)
     return non_replyed_tweets
 
+
 def exclude_replyed(tweets: List[dict]):
     return exclude_liked(tweets) # いいねしたもの = リプライ済 としている
+
+
+if __name__ == "__main__":
+    print(is_liked(My.id, { "id": some_tweets_data["data"][3]["id"]}))
+
---
+++ search_liked_users.py
@@ -0,0 +1,51 @@
+# https://github.com/twitterdev/Twitter-API-v2-sample-code/blob/main/Likes-Lookup/liking_users.py
+
+import requests
+import json
+from config import *
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = BEARER_TOKEN
+
+
+def create_url(tweet_id: str):
+    user_fields = "user.fields=created_at,description"
+    url = f"https://api.twitter.com/2/tweets/{tweet_id}/liking_users?max_results=100&expansions=pinned_tweet_id"
+    return url, user_fields
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2LikingUsersPython"
+    return r
+
+
+def connect_to_endpoint(url, user_fields):
+    response = requests.request("GET", url, auth=bearer_oauth, params=user_fields)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(
+                response.status_code, response.text
+            )
+        )
+    return response.json()
+
+
+def search_liked_users(tweet_id: str):
+    url, tweet_fields = create_url(tweet_id)
+    json_response = connect_to_endpoint(url, tweet_fields)
+    print("search_liked_users's result:")
+    print(json.dumps(json_response, indent=4, sort_keys=True, ensure_ascii=False))
+    if "data" not in json_response:
+        return []
+    return [user["id"] for user in json_response["data"]]
+
+
+if __name__ == "__main__":
+    print(search_liked_users("1466062033967738880")) # 「俺だけしかいないアドカレ。」
