877aae7ba573ebdbd81000d983aa9828fff9385c
dai <daichi.iphone@icloud.com>
2022-05-27 01:32:58 +0900

    リファクタ：仮引数名を前回の修正と一致させた

--- add_header_text.py
+++ add_header_text.py
@@ -1,7 +1,7 @@
 
-def add_header_text(main_text: str):
-    header = "ランダムに選ばれた{}はこれです！\n".format("数字" if main_text.isdecimal() else "行")
-    ret = header + main_text
+def add_header_text(choiced_text: str):
+    header = "ランダムに選ばれた{}はこれです！\n".format("数字" if choiced_text.isdecimal() else "行")
+    ret = header + choiced_text
     if(len(ret)>140):
         ret = ret[:137] + "..."
     assert(len(ret)<=140)
